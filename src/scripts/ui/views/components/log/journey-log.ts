44d096b6c2c791c596c1ce46da3587a1b5adc33c
Riley Ezra Lunaris <riley.lunaris@gmail.com>
2026-02-23 02:21:22 -0600

    Addedd Journey Log

---
+++ src/scripts/ui/views/components/log/journey-log.ts
@@ -0,0 +1,43 @@
+import { Component } from "../component";
+
+export class JourneyLog extends Component {
+    header: HTMLHeadingElement;
+    output: HTMLDivElement;
+
+    constructor(
+        log_id: string,     // delete later after refactor
+        output_id: string,  // delete later after refactor
+    ) {
+        super(document.createElement("div"));
+        this.root.id = log_id; // "log-container";
+        this.root.className = "mt-8 pt-4 border-t"
+
+        // Components
+        this.header = Object.assign(document.createElement("h4"), {
+            className: "text-lg font-semibold text-gray-800 mb-2",
+            textContent: "Journey Log",
+        })
+        this.output = Object.assign(document.createElement("div"), {
+            id: output_id, // "log-area", 
+            className: "h-32 overflow-y-scroll bg-gray-50 p-3 rounded border",
+        })
+
+        // Build Dom
+        this.root.replaceChildren(
+            this.header,
+            this.output,
+        )
+    }
+
+    public set_label(label:string) {
+        this.header.textContent = label;
+    }
+
+    public set_value(event:string) {
+        this.output.prepend(event);
+    }
+
+    public clear() {
+        this.output.innerHTML = "";
+    }
+}
