40bb84214eaa9c3d6ea15324529ade2d5d49e514
Riley Ezra Lunaris <riley.lunaris@gmail.com>
2026-03-24 11:26:58 -0600

    refactored eventname construction to be scalable.

--- src/events/types.ts
+++ src/events/types.ts
@@ -3,8 +3,32 @@
 //                                  Event Types
 // ═══════════════════════════════════════════════════════════════════════════
 
-import { GameEvents, UserEvents } from "."
+import { 
+    GameEvents, 
+    UserEvents, 
+    PlayerEvents, 
+    ScenarioEvents,
+} from "./def"
 
+
+/**
+ * Collection of all events within the app.
+ */
+type EventGroups = 
+| typeof GameEvents 
+| typeof UserEvents 
+| typeof PlayerEvents 
+| typeof ScenarioEvents
+
+/**
+ * Utility type that extracts the union of all values from an object type.
+ */
+type ValueOf<T> = T[keyof T]
+
+/**
+ * Mapped Name type for all events within the app.
+ */
+export type EventName = ValueOf<EventGroups>
 
 /**
  * Default structure for Event Handlers and optional data.
@@ -13,16 +37,6 @@ import { GameEvents, UserEvents } from "."
  * during a subscribed event flag.
  */
 export type EventHandler <T = any> = (data: T) => void
-
-
-/**
- * Mapped Names for all events within the app.
- * Includes events from the game, the api, and the user interfaces.
- */
-export type EventName = 
-| typeof GameEvents[keyof typeof GameEvents]
-| typeof UserEvents[keyof typeof UserEvents]
-
 
 /**
  * An Event subscription typing which links and event with a handler.
