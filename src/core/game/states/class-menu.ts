7f48b2628f145494971c3cbd32ef0f8cd304de2b
Riley Ezra Lunaris <riley.lunaris@gmail.com>
2026-03-26 16:40:42 -0600

    fixed class menu flag and new state

--- src/core/game/states/class-menu.ts
+++ src/core/game/states/class-menu.ts
@@ -6,7 +6,7 @@
 import { GameEvents, UserEvents } from "@/events"
 import { GameState } from "./state"
 import { ListRandomProfessions } from "@/scripts/game/content/professions"
-import { Playing } from "./playing"
+import { Hired } from "./hired"
 
 
 export class CharacterSelection extends GameState {
@@ -14,14 +14,14 @@ export class CharacterSelection extends GameState {
         this.subscriptions.push(
             this.context.bus.subscribe(
                 UserEvents.chose_class,
-                (profession_id) => {this.context.changeState(new Playing(this.context, profession_id))}
+                (profession_id) => {this.context.changeState(new Hired(this.context, profession_id))}
             )
         )
     }
     
     post(): void {
         this.context.bus.broadcast(
-            GameEvents.choose_class,
+            GameEvents.unemployed,
             ListRandomProfessions()
         )
     }
