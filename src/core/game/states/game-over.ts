60ca6cfe6b719c0d73d8a690f3fd4b8a9d925775
Riley Ezra Lunaris <riley.lunaris@gmail.com>
2026-03-26 16:39:20 -0600

    refactored game over state

--- src/core/game/states/game-over.ts
+++ src/core/game/states/game-over.ts
@@ -4,12 +4,11 @@
 // ═══════════════════════════════════════════════════════════════════════════
 
 import { GameEvents } from "@/events"
-import type { GameContext } from "../context"
 import { GameState } from "./state"
 
 
 export class GameOver extends GameState {
-    enter (context: GameContext) {
-        context.bus.broadcast(GameEvents.game_over)
+    enter () {
+        this.context.bus.broadcast(GameEvents.game_over)
     }
 }
