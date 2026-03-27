396877725554486ba8309b77964516396e6b7731
Riley Ezra Lunaris <riley.lunaris@gmail.com>
2026-03-27 14:12:50 -0600

    added User States

--- src/events/def/user.ts
+++ src/events/def/user.ts
@@ -11,6 +11,7 @@
  * within the application layer.
  */
 export const UserEvents = {
+
     /** User wants to start the game. */
     started_game: "user.started_game",
 
@@ -20,6 +21,16 @@ export const UserEvents = {
     /** User chose a difficulty. */
     chose_difficulty: "user.chose_difficulty",
 
+    /** Accepted Job Offer. */
+    accepted_offer: "user.accepted_offer",
+
+    /** Rejected job Offer. */
+    rejected_offer: "user.rejected_offer",
+
     /** User chose an option from the scenario. */
     chose_scenario_option: "user.chose_scenario_option",
+
+    /** User went back. */
+    went_back: "user.went_back"
+    
 } as const
