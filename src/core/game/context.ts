c45e6751c3949209c62f8c341e0893870b7750b0
Riley Ezra Lunaris <riley.lunaris@gmail.com>
2026-03-27 14:29:23 -0600

    added jobs, and last variables for context. added revert state method.

--- src/core/game/context.ts
+++ src/core/game/context.ts
@@ -6,11 +6,15 @@
 import type { EventBus } from "@/events";
 import type { GameEngine } from "./engine";
 import type { GameState } from "./states";
+import { ListRandomProfessions } from "@/scripts/game/content/professions";
+import type { Profession } from "@/scripts/game/types";
 
 
 export class GameContext {
     engine: GameEngine
     bus: EventBus
+    jobs: Profession[]
+    last?: GameState
 
     constructor (
         engine: GameEngine,
@@ -18,9 +22,15 @@ export class GameContext {
     ) {
         this.engine = engine
         this.bus = event_bus
+        this.jobs = ListRandomProfessions()
     }
 
     changeState (new_state: GameState): void {
+        this.last = this.engine.state
         this.engine.changeState(new_state)
+    }
+
+    revertState () {
+        if (this.last) this.engine.changeState(this.last)
     }
 }
