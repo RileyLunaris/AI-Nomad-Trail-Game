0ee47b1b38018e439ec0a877dbf7a48aae6d116d
Riley Ezra Lunaris <riley.lunaris@gmail.com>
2026-03-27 14:26:11 -0600

    modified helpers, added job offer, and last page, fixed bus subscriptions

--- src/ui/controller.ts
+++ src/ui/controller.ts
@@ -3,9 +3,11 @@
 //                              Screen Controller
 // ═══════════════════════════════════════════════════════════════════════════
 
-import { GameEvents, type EventBus } from "@/events";
+import { GameEvents, PlayerEvents, type EventBus } from "@/events";
 import { ClassSelectionScreen, StartScreen } from "./screens";
 import type { ScreenManager } from ".";
+import { JobOfferScreen } from "./screens/job-offer-screen";
+import type { Profession } from "@/scripts/game/types";
 
 
 export class ScreenController {
@@ -24,6 +26,7 @@ export class ScreenController {
         this.manager = manager
     }
     
+
     // #endregion
     // ───────────────────────────────────────────────────────────────────────
     // #region helpers
@@ -31,14 +34,24 @@ export class ScreenController {
 
     private start_screen = () => {
         this.manager.clear()
-        this.manager.replace(new StartScreen(this.bus))
+        if (!this.manager.current_screen) {
+            this.manager.push(new StartScreen(this.bus))
+        }
     }
 
     private class_selection_screen = () => {
-        this.manager.replace(new ClassSelectionScreen(this.bus))
+        this.manager.push(new ClassSelectionScreen(this.bus))
     }
 
+    private job_offer_screen = (profession: Profession) => {
+        this.manager.push(new JobOfferScreen(this.bus, profession))
+    }
 
+    private go_to_last_page = () => {
+        this.manager.pop() 
+    }
+
+    
     // #endregion
     // ───────────────────────────────────────────────────────────────────────
     // #region Public API
@@ -46,7 +59,10 @@ export class ScreenController {
 
     public subscribe () {
         this.bus.subscribe(GameEvents.started, this.start_screen)
-        this.bus.subscribe(GameEvents.unemployed, this.class_selection_screen)
+        this.bus.subscribe(PlayerEvents.unemployed, this.class_selection_screen)
+        this.bus.subscribe(PlayerEvents.offered_job, this.job_offer_screen)
+        this.bus.subscribe(PlayerEvents.rejected_offer, this.go_to_last_page)
+        // this.bus.subscribe(PlayerEvents.was_hired, this.journey_screen)
     }
 
     public unsubscribe () {
