eadab1b963794422cd55f294ee257c9c2feb7600
Riley Ezra Lunaris <riley.lunaris@gmail.com>
2026-03-27 14:12:39 -0600

    added Player States

--- src/events/def/player.ts
+++ src/events/def/player.ts
@@ -11,6 +11,23 @@
  * player's status and states within the game.
  */
 export const PlayerEvents = {
+
+    /** The player needs a job. */
+    unemployed: "player.seeking_employment",
+    
+    /** The player was offered a job. */
+    offered_job: "player.offered_job",
+
+    /** Player accepted the offer */
+    accepted_offer: "player.accepted_offer",
+
+    /** Player rejected the offer. */
+    rejected_offer: "player.rejected_offer",
+
+    /** The player accepted the offer. */
+    was_hired: "player.was_hired",
+
     /** The player's stats were affected.  */
     update: "player.update",
+    
 } as const
