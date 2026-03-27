a08926081c38cc1b3c85420691df4d787f279d09
Riley Ezra Lunaris <riley.lunaris@gmail.com>
2026-03-27 14:26:56 -0600

    renamed hired game state to job-offer

---
+++ src/core/game/states/job-offer.ts
@@ -0,0 +1,56 @@
+
+// ═══════════════════════════════════════════════════════════════════════════
+//                          Game State : Offered Job
+// ═══════════════════════════════════════════════════════════════════════════
+
+import { PlayerEvents, UserEvents } from "@/events"
+import { GameState } from "./state"
+import type { Profession } from "@/scripts/game/types"
+import type { GameContext } from "../context"
+import { ClassMenu } from "./class-menu"
+import { GameOver } from "./game-over"
+
+
+export class JobOffer extends GameState {
+    private profession: Profession
+
+    constructor (
+        context: GameContext,
+        profession: Profession,
+    ) {
+        super(context)
+        this.profession = profession
+    }
+
+
+    private reject_offer = () => {
+        this.context.jobs = this.context.jobs.filter(job => job != this.profession)
+        if (this.context.jobs.length === 0) {
+            this.context.changeState(new GameOver(this.context))
+        } else {
+            this.context.changeState(new ClassMenu(this.context))
+        }
+    }
+
+    private accept_offer = () => {
+        this.context.bus.broadcast(PlayerEvents.accepted_offer, this.profession)
+    }
+
+    listen(): void {
+        this.subscriptions.push(
+            this.context.bus.subscribe(
+                UserEvents.rejected_offer, 
+                this.reject_offer
+            ),
+            this.context.bus.subscribe(
+                UserEvents.accepted_offer,
+                this.accept_offer
+            ),
+        )
+
+    }
+
+    post () {
+        this.context.bus.broadcast(PlayerEvents.offered_job, this.profession)
+    }
+}
